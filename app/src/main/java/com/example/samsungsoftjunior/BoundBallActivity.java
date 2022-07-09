eb1188f001d41c6e1086405ff5634a01e8621949
kzming <kzming2007@gmail.com>
2022-07-10 03:08:23 +0900

    벽에 튕기는 공 만듬

--- app/src/main/java/com/example/samsungsoftjunior/BoundBallActivity.java
+++ app/src/main/java/com/example/samsungsoftjunior/BoundBallActivity.java
@@ -35,19 +35,29 @@ public class BoundBallActivity extends AppCompatActivity {
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point(); display.getRealSize(size);
         screenWidth = size.x; // 화면의 너비
-        screenHeight = size.y; // 화면의 높이
+        screenHeight = size.y - 800; // 화면의 높이
     }
 
+int a = 0;
     private void setTimer() {
         handler = new Handler(Looper.getMainLooper());
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    myImage.setY(myImage.getY() + 10); // 1) 이미지뷰의 좌표를 현재의 좌표를 가져와서 +10 설정
+                    //myImage.setY(myImage.getY() + 10); // 1) 이미지뷰의 좌표를 현재의 좌표를 가져와서 +10 설정
                     // 위의 명령 대신 다음과 같은 작업이 필요.
 
                     // if 현재의 Y 좌표가 0보다 크고 화면 높이 보다 변화값을 증가
+                    if(a == 1 && myImage.getY() < 0) {
+                        a -= 1;
+                    }else if(a == 1) {
+                        myImage.setY(myImage.getY() - 10);
 
+                    }else if(myImage.getY() >= screenHeight) {
+                        a += 1;
+                    }else if(a == 0) {
+                        myImage.setY(myImage.getY() + 10);
+                    }//이게되네ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
                     // else if 현재의 Y 좌표가 화면의 높이보다 크면 변화값을 감소
 
                     // 생각해볼 부분 : 어떻게 방향성을 유지 할것인가?
